cc83961c8bb824d5b8ef565b7693138dbcb0775b
Jahnavi <36070746+npjahnavi@users.noreply.github.com>
2022-11-10 20:08:17 -0500

    Update chess.js

--- pages/chess.js
+++ pages/chess.js
@@ -1,4 +1,14 @@
 import styles from '../styles/Chess.module.css';
+//export default function ChessPage() {
+  //return <div>Testing New Page</div>;
+//}
+
 export default function ChessPage() {
-  return <div>Testing New Page</div>;
+  // Tell React to put a div down that is
+  // controlled by the makeGame function
+  return <div ref={makeGame} />;
+}
+
+function makeGame(div) {
+  div.innerText = 'content now controlled by makeGame';
 }
